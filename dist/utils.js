4e44128478cc67217ac194fa4b36ac5f3731b3d1
Lucho <lzamora@effectussoftware.com>
2020-06-02 14:18:44 -0300

    dependencies fixed

--- dist/utils.js
+++ dist/utils.js
@@ -1,15 +1,14 @@
 export var getRotationDegrees = function (prizeNumber, numberOfPrizes) {
-  var degreesPerPrize = 360 / numberOfPrizes
-  var initialRotation = 43 + degreesPerPrize / 2
-  var randomDifference = (-1 + Math.random() * 2) * degreesPerPrize * 0.35
-  var prizeRotation =
-    degreesPerPrize * (numberOfPrizes - prizeNumber) -
-    initialRotation +
-    randomDifference
-  return numberOfPrizes - prizeNumber > numberOfPrizes / 2
-    ? -360 + prizeRotation
-    : prizeRotation
-}
+    var degreesPerPrize = 360 / numberOfPrizes;
+    var initialRotation = 43 + degreesPerPrize / 2;
+    var randomDifference = (-1 + Math.random() * 2) * degreesPerPrize * 0.35;
+    var prizeRotation = degreesPerPrize * (numberOfPrizes - prizeNumber) -
+        initialRotation +
+        randomDifference;
+    return numberOfPrizes - prizeNumber > numberOfPrizes / 2
+        ? -360 + prizeRotation
+        : prizeRotation;
+};
 export var clamp = function (min, max, val) {
-  return Math.min(Math.max(min, +val), max)
-}
+    return Math.min(Math.max(min, +val), max);
+};
