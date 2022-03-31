46219c3d2336dfe8c81977930da7d9328be759d3
Gaston-Gonzalez <ggonzalez@effectussoftware.com>
2022-03-31 15:51:12 -0300

    Example fix

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -14,12 +14,6 @@ const data = [
   { option: 'WHEEL' },
 ];
 
-const data2 = [
-  { option: 'REACT 0' },
-  { option: 'CUSTOM 1' },
-  { option: 'ROULETTE 2', style: { textColor: '#f9dd50' } },
-];
-
 const backgroundColors = ['#ff8f43', '#70bbe0', '#0b3351', '#f9dd50'];
 const textColors = ['#0b3351'];
 const outerBorderColor = '#eeeeee';
@@ -42,104 +36,34 @@ const App = () => {
     setPrizeNumber(newPrizeNumber);
     setMustSpin(true);
   };
-  const [mustSpin2, setMustSpin2] = useState(false);
-  const [prizeNumber2, setPrizeNumber2] = useState(0);
-
-  const handleSpinClick2 = () => {
-    const newPrizeNumber = 1;
-    setPrizeNumber2(newPrizeNumber);
-    setMustSpin2(true);
-  };
-  const [mustSpin3, setMustSpin3] = useState(false);
-  const [prizeNumber3, setPrizeNumber3] = useState(0);
-
-  const handleSpinClick3 = () => {
-    const newPrizeNumber = 2;
-    setPrizeNumber3(newPrizeNumber);
-    setMustSpin3(true);
-  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <div>
-          <Wheel
-            mustStartSpinning={mustSpin}
-            prizeNumber={prizeNumber}
-            data={data}
-            backgroundColors={backgroundColors}
-            textColors={textColors}
-            fontSize={fontSize}
-            outerBorderColor={outerBorderColor}
-            outerBorderWidth={outerBorderWidth}
-            innerRadius={innerRadius}
-            innerBorderColor={innerBorderColor}
-            innerBorderWidth={innerBorderWidth}
-            radiusLineColor={radiusLineColor}
-            radiusLineWidth={radiusLineWidth}
-            spinDuration={spinDuration}
-            // perpendicularText
-            textDistance={textDistance}
-            onStopSpinning={() => {
-              setMustSpin(false);
-            }}
-          />
-          <button className={'spin-button'} onClick={handleSpinClick}>
-            SPIN
-          </button>
-        </div>
-        <div>
-          <Wheel
-            mustStartSpinning={mustSpin2}
-            prizeNumber={prizeNumber2}
-            data={data2}
-            backgroundColors={backgroundColors}
-            textColors={textColors}
-            fontSize={fontSize}
-            outerBorderColor={outerBorderColor}
-            outerBorderWidth={outerBorderWidth}
-            innerRadius={innerRadius}
-            innerBorderColor={innerBorderColor}
-            innerBorderWidth={innerBorderWidth}
-            radiusLineColor={radiusLineColor}
-            radiusLineWidth={radiusLineWidth}
-            spinDuration={spinDuration}
-            // perpendicularText
-            textDistance={textDistance}
-            onStopSpinning={() => {
-              setMustSpin2(false);
-            }}
-          />
-          <button className={'spin-button'} onClick={handleSpinClick2}>
-            SPIN
-          </button>
-        </div>
-        <div>
-          <Wheel
-            mustStartSpinning={mustSpin3}
-            prizeNumber={prizeNumber3}
-            data={data}
-            backgroundColors={backgroundColors}
-            textColors={textColors}
-            fontSize={fontSize}
-            outerBorderColor={outerBorderColor}
-            outerBorderWidth={outerBorderWidth}
-            innerRadius={innerRadius}
-            innerBorderColor={innerBorderColor}
-            innerBorderWidth={innerBorderWidth}
-            radiusLineColor={radiusLineColor}
-            radiusLineWidth={radiusLineWidth}
-            spinDuration={spinDuration}
-            // perpendicularText
-            textDistance={textDistance}
-            onStopSpinning={() => {
-              setMustSpin3(false);
-            }}
-          />
-          <button className={'spin-button'} onClick={handleSpinClick3}>
-            SPIN
-          </button>
-        </div>
+        <Wheel
+          mustStartSpinning={mustSpin}
+          prizeNumber={prizeNumber}
+          data={data}
+          backgroundColors={backgroundColors}
+          textColors={textColors}
+          fontSize={fontSize}
+          outerBorderColor={outerBorderColor}
+          outerBorderWidth={outerBorderWidth}
+          innerRadius={innerRadius}
+          innerBorderColor={innerBorderColor}
+          innerBorderWidth={innerBorderWidth}
+          radiusLineColor={radiusLineColor}
+          radiusLineWidth={radiusLineWidth}
+          spinDuration={spinDuration}
+          // perpendicularText
+          textDistance={textDistance}
+          onStopSpinning={() => {
+            setMustSpin(false);
+          }}
+        />
+        <button className={'spin-button'} onClick={handleSpinClick}>
+          SPIN
+        </button>
       </header>
     </div>
   );
